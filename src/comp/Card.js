e0b2071b8be7394806a6cc2873303a23c86f95a9
mepriyanshuhoo <priyanshu10hero@gmail.com>
2022-12-14 22:09:27 +0530

    changes in PDF API call

--- src/comp/Card.js
+++ src/comp/Card.js
@@ -3,7 +3,7 @@ function Card(props) {
     return (
         <div>
             <h1>{Cardinfo.task_name}</h1>
-            <div style={{ color: "red" }}>{Cardinfo.task_status}</div>
+            <div style={{ color:"wheat" }}>{Cardinfo.task_status}</div>
             <button
                 value={props.index}
                 name={Cardinfo.task_uuid}
